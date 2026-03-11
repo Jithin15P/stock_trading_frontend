bc41240e5f318ca0df0300de77b40336a6002106
JITHIN LAKSHMAN <158822916+Jithin15P@users.noreply.github.com>
2026-03-11 22:41:17 +0530

    Update CreateTicket.js

--- src/landingpage/Support/CreateTicket.js
+++ src/landingpage/Support/CreateTicket.js
@@ -10,11 +10,11 @@ function CreateTicket() {
                    <i className="fa-solid fa-circle-plus"></i> Account Opening
                 </h4>
                 <ul>
-                <li><a href=""style={{textDecoration:"none"}}>Resident individual</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Minor</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Non Resident Indian (NRI)</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Company, Partnership, HUF and LLP</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Glossary</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Resident individual</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Minor</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Non Resident Indian (NRI)</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Company, Partnership, HUF and LLP</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Glossary</a></li>
                 </ul>
             </div>
               <div className='col-4 p-3  '>
@@ -22,11 +22,11 @@ function CreateTicket() {
                    <i class="fa-solid fa-user"></i> Your Zerodha Account
                 </h4>
                 <ul>
-                <li><a href=""style={{textDecoration:"none"}}>Your Profile</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Account modification</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Client Master Report (CMR) and Depository Participant (DP)</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Nomination</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Transfer and conversion of securities</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Your Profile</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Account modification</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Client Master Report (CMR) and Depository Participant (DP)</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Nomination</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Transfer and conversion of securities</a></li>
                 </ul>
             </div>
               <div className='col-4 p-3  '>
@@ -34,43 +34,43 @@ function CreateTicket() {
                   <i class="fa-solid fa-chart-simple"></i> Kite
                 </h4>
                 <ul>
-                <li><a href=""style={{textDecoration:"none"}}>IPO</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Trading FAQs</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Margin Trading Facility (MTF) and Margins</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Charts and orders</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Alerts and Nudges</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>General</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>IPO</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Trading FAQs</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Margin Trading Facility (MTF) and Margins</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Charts and orders</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Alerts and Nudges</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>General</a></li>
                 </ul>
             </div>
             <div className='col-4 p-3 '>
                 <h4 className='fs-5' style={{fontWeight:400}}>
                   <i class="fas fa-credit-card"></i> Funds
                 </h4>
-                <li><a href=""style={{textDecoration:"none"}}>Add money</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Withdraw money</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Add bank accounts</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>eMandates</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Add money</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Withdraw money</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Add bank accounts</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>eMandates</a></li>
             </div>
             <div className='col-4 p-3 '>
                 <h4 className='fs-5'style={{fontWeight:400}}>
                   <i class="fas fa-terminal"></i> Console
                 </h4>
-                <li><a href=""style={{textDecoration:"none"}}>Portfolio</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Corporate actions</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Funds statement</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Reports</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Profile</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Segments</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Portfolio</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Corporate actions</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Funds statement</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Reports</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Profile</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Segments</a></li>
             </div>
             <div className='col-4 p-3 '>
                 <h4 className='fs-5'style={{fontWeight:400}}>
                  <i class="fa-solid fa-coins"></i> Coin
                 </h4>
-                <li><a href=""style={{textDecoration:"none"}}>Mutual funds</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>National Pension Scheme (NPS)</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Features on Coin</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>Payments and Orders</a></li>
-                <li><a href=""style={{textDecoration:"none"}}>General</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Mutual funds</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>National Pension Scheme (NPS)</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Features on Coin</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>Payments and Orders</a></li>
+                <li><a href="https://wayaway-production.up.railway.app/"style={{textDecoration:"none"}}>General</a></li>
             </div>
             </div>
         </div>
